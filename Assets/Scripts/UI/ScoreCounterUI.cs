e63e356cf1c9bd28eee130eafb45474d6b6053d2
tontonpiero <piero@gerwinsoftware.com>
2022-08-03 19:38:37 +0200

    smooth increment score counter

--- Assets/Scripts/UI/ScoreCounterUI.cs
+++ Assets/Scripts/UI/ScoreCounterUI.cs
@@ -9,20 +9,40 @@ namespace Solitary.UI
         [SerializeField] private GameManager gameManager;
         [SerializeField] private TMP_Text textCounter;
 
+        private float currentScore = 0f;
+        private int targetScore = 0;
+        private float velocity = 0f;
+
         private void Start()
         {
             gameManager.OnScoreChanged += OnGameScoreChanged;
-            UpdateCounterText();
+            targetScore = gameManager.GetScore();
+            currentScore = gameManager.GetScore();
         }
 
         private void OnGameScoreChanged()
         {
-            UpdateCounterText();
+            targetScore = gameManager.GetScore();
+        }
+
+        private void Update()
+        {
+            if (currentScore < targetScore)
+            {
+                currentScore = Mathf.SmoothDamp(currentScore, targetScore, ref velocity, 0.1f);
+                if (currentScore > targetScore) currentScore = targetScore;
+                UpdateCounterText();
+            }
+            else if (currentScore > targetScore)
+            {
+                currentScore = targetScore;
+                UpdateCounterText();
+            }
         }
 
         private void UpdateCounterText()
         {
-            textCounter.text = gameManager.GetScore().ToString();
+            textCounter.text = Mathf.RoundToInt(currentScore).ToString();
         }
     }
 }
